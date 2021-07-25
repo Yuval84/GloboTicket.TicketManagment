c5dea5ba73c9353aa1d4194886afeeb69612d58f
Yuval Idelman <yuvale84@gmail.com>
2021-07-25 17:04:42 +0300

    Identity class and authenticationServices

---
+++ GloboTicket.TicketManagement.App/Services/Base/BaseDataService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using GloboTicket.TicketManagement.App.Serviced;
+
+namespace GloboTicket.TicketManagement.App.Services.Base
+{
+    public class BaseDataService
+    {
+        protected readonly ILocalStorageService _localStorage;
+
+        protected IClient _client;
+
+        public BaseDataService(ILocalStorageService localStorage, IClient client)
+        {
+            _localStorage = localStorage;
+            _client = client;
+        }
+
+        protected ApiResponse<Guid> ConvertApiExceptions<Guid>(ApiException ex)
+        {
+            if (ex.StatusCode == 400)
+            {
+                return new ApiResponse<Guid>() { Message = "Validation errors have occurred.", ValidationErrors = ex.Response, Success = false };
+            }
+            else if (ex.StatusCode == 404)
+            {
+                return new ApiResponse<Guid>() { Message = "The requested item could not be found.", Success = false };
+            }
+            else
+            {
+                return new ApiResponse<Guid>() { Message = "Something went wrong, please try again.", Success = false };
+            }
+        }
+
+        protected async Task AddBearerToken()
+        {
+            if (await _localStorage.ContainKeyAsync("token"))
+                _client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await _localStorage.GetItemAsync<string>("token"));
+        }
+
+
+
+
+
+
+
+    }
+
+}
---
+++ GloboTicket.TicketManagement.App/Services/Base/Client.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace GloboTicket.TicketManagement.App.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient => _httpClient;
+    }
+}
